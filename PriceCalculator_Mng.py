e96d71a47300104274a26ed81304e7adc97850d1
tiwobot <twogun.pre@gmail.com>
2023-04-19 14:38:20 -0700

    Develop MNG Calculator further

--- PriceCalculator_Mng.py
+++ PriceCalculator_Mng.py
@@ -5,7 +5,10 @@ from selenium.webdriver.support.wait import WebDriverWait
 import common
 from dependencies import ChromeDriverLocation
 
-
+FromSearch = common.FromProvince
+ToSearch= common.ToProvince
+FromCityString = (common.FromProvince+" - "+common.FromCounty).upper().replace("I", "İ")
+ToCityString = (common.ToProvince+" - "+common.ToCounty).upper().replace("I", "İ")
 desiWeight = common.desiKG
 desiWidth = common.desiWidth
 desiLength = common.desiLength
@@ -19,3 +22,14 @@ driver.implicitly_wait(2)
 
 title = driver.title
 print(title)
+print(FromCityString)
+
+driver.find_element(By.CSS_SELECTOR, ".closed").click()
+
+driver.find_element(By.ID, "form_locationFirst").send_keys(FromSearch)
+driver.find_element(By.LINK_TEXT, FromCityString).click()
+driver.find_element(By.ID, "form_locationLast").send_keys(ToSearch)
+driver.find_element(By.LINK_TEXT, ToCityString).click()
+driver.find_element(By.CSS_SELECTOR, ".linkBox:nth-child(3) span:nth-child(1)").click()
+driver.find_element(By.CSS_SELECTOR, ".packageParcel a").click()
+
