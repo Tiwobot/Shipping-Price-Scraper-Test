967d853b0cafa8d9f210da78b8f4a025bd6bb477
tiwobot <twogun.pre@gmail.com>
2023-04-19 12:18:39 -0700

    Clean and reformat the codes

--- PriceCalculator_Yurtici.py
+++ PriceCalculator_Yurtici.py
@@ -5,8 +5,10 @@ from selenium.webdriver.support.wait import WebDriverWait
 import common
 from dependencies import ChromeDriverLocation
 
-SearchSourceCityandCountry=common.FromProvince+" "+common.FromCounty+" "+common.FromDetails
-SearchDestinationCityandCountry=common.ToProvince+" "+common.ToCounty+" "+common.ToDetails
+SearchSourceCityandCountry = common.FromProvince + \
+    " "+common.FromCounty+" "+common.FromDetails
+SearchDestinationCityandCountry = common.ToProvince + \
+    " "+common.ToCounty+" "+common.ToDetails
 desiWeight = common.desiKG
 desiWidth = common.desiWidth
 desiLength = common.desiLength
@@ -21,16 +23,24 @@ driver.implicitly_wait(2)
 title = driver.title
 print(title)
 
-driver.find_element(By.ID, "SourceCityAndCounty").send_keys(SearchSourceCityandCountry)
+driver.find_element(By.ID, "SourceCityAndCounty").send_keys(
+    SearchSourceCityandCountry)
 driver.find_element(By.CSS_SELECTOR, "em:nth-child(3)").click()
-driver.find_element(By.ID, "DestinationCityAndCounty").send_keys(SearchDestinationCityandCountry)
+driver.find_element(By.ID, "DestinationCityAndCounty").send_keys(
+    SearchDestinationCityandCountry)
 driver.find_element(By.CSS_SELECTOR, "em:nth-child(2)").click()
 driver.find_element(By.ID, "button-select-address").click()
 driver.find_element(By.CSS_SELECTOR, ".package > svg").click()
-driver.find_element(By.XPATH, "(//input[@type=\'number\'])[5]").send_keys(desiWidth)
-driver.find_element(By.XPATH, "(//input[@type=\'number\'])[6]").send_keys(desiLength)
-driver.find_element(By.XPATH, "(//input[@type=\'number\'])[7]").send_keys(desiHeight)
-driver.find_element(By.XPATH, "(//input[@type=\'number\'])[8]").send_keys(desiWeight)
+driver.find_element(
+    By.XPATH, "(//input[@type=\'number\'])[5]").send_keys(desiWidth)
+driver.find_element(
+    By.XPATH, "(//input[@type=\'number\'])[6]").send_keys(desiLength)
+driver.find_element(
+    By.XPATH, "(//input[@type=\'number\'])[7]").send_keys(desiHeight)
+driver.find_element(
+    By.XPATH, "(//input[@type=\'number\'])[8]").send_keys(desiWeight)
 driver.find_element(By.ID, "btn-price-calculation").click()
-print(driver.find_element(By.CSS_SELECTOR, "#product-genaral-total > b:nth-child(1)").text)
-print(driver.find_element(By.CSS_SELECTOR, "#product-genaral-total > b:nth-child(3)").text)
+print(driver.find_element(By.CSS_SELECTOR,
+      "#product-genaral-total > b:nth-child(1)").text)
+print(driver.find_element(By.CSS_SELECTOR,
+      "#product-genaral-total > b:nth-child(3)").text)
